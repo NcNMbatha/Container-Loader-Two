ee4c16029ba567f97feeeccf70d5e8868b138208
Njabulo Mbatha <njabulo.mbatha@kcpivot.co.za>
2022-06-17 14:44:09 +0200

    first commit

---
+++ ContainerLoaderTwo/ContainerLoader.cs
@@ -0,0 +1,62 @@
+﻿namespace ContainerLoaderTwo
+{
+    public class ContainerLoader
+    {
+
+        private List<Stack<char>> listOfContainerStacks;
+        private Stack<char> containerStack;
+
+        public ContainerLoader()
+        {
+            listOfContainerStacks = new List<Stack<char>>();
+            containerStack = new Stack<char>();
+        }
+
+        public int GetMinimunNumberOfContainerStacks(string containers)
+        {
+            if (string.IsNullOrEmpty(containers))
+            {
+                return 0;
+            }
+
+            return GetNumberOfStacks(containers);
+
+        }
+
+        public int GetNumberOfStacks(string containerArrival)
+        {
+            for (int letterIndex = 0; letterIndex < containerArrival.Length; letterIndex++)
+            {
+                if (letterIndex == 0)
+                {
+                    containerStack.Push(containerArrival[letterIndex]);
+                    listOfContainerStacks.Add(containerStack);
+                }
+                else
+                {
+                    AddToTheRightContainerStack(containerArrival, containerArrival[letterIndex]);
+                }
+            }
+
+            return listOfContainerStacks.Count;
+        }
+
+        public void AddToTheRightContainerStack(string containerToStack, char letterToStack)
+        {
+            for (int stackIndex = 0; stackIndex < listOfContainerStacks.Count; stackIndex++)
+            {
+                if (listOfContainerStacks[stackIndex].Peek() >= letterToStack )
+                {
+                    listOfContainerStacks[stackIndex].Push(letterToStack);
+                    break;
+                }
+                if (letterToStack > listOfContainerStacks[stackIndex].Peek())
+                {
+                    containerStack.Push(letterToStack);
+                    listOfContainerStacks.Add(containerStack);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ContainerLoaderTwoTest/ContainerLoaderTest.cs
@@ -0,0 +1,60 @@
+﻿using ContainerLoaderTwo;
+using NUnit.Framework;
+
+namespace ContainerLoaderTwoTest
+{
+    public class ContainerLoaderTest
+    {
+        private ContainerLoader _containerLoader;
+
+        [SetUp]
+        public void Setup()
+        {
+            _containerLoader = new ContainerLoader();
+        }
+
+        [Test]
+        public void Given_OneContainer_When_CountingNumberOfStacks_Returns_One()
+        {
+            var containerInput = "A";
+            var expected = 1;
+
+            var actual = _containerLoader.GetNumberOfStacks(containerInput);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Given_ContainerLettersInSortedDescendingOrder_When_CountingNumberOfStacks_Return_One()
+        {
+            var containerInput = "CCCCBBBBAAAA";
+            var expected = 1;
+
+            var actual = _containerLoader.GetNumberOfStacks(containerInput);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Given_ContainerLetters_When_CountingNumberOfStacks_Returns_Four()
+        {
+            var containerInput = "ACMICPC";
+            var expected = 4;
+
+            var actual = _containerLoader.GetNumberOfStacks(containerInput);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Given_ContainersLetters_When_CountingNumberOfStacks_Returns_Three()
+        {
+            var containerInput = "CBACBACBACBACBA";
+            var expected = 3;
+
+            var actual = _containerLoader.GetNumberOfStacks(containerInput);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
